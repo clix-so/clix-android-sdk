78c0e0143f6bb6e5858afe72576827f455b96425
Minkyu Cho <minkyu@clix.so>
2025-09-22 22:28:37 +0900

    feat: support datetime property (#3)

--- clix/src/main/kotlin/so/clix/models/ClixUserProperty.kt
+++ clix/src/main/kotlin/so/clix/models/ClixUserProperty.kt
@@ -1,5 +1,13 @@
 package so.clix.models
 
+import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Calendar
+import java.util.Date
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -12,9 +20,28 @@ internal data class ClixUserProperty(
         USER_PROPERTY_TYPE_STRING,
         USER_PROPERTY_TYPE_NUMBER,
         USER_PROPERTY_TYPE_BOOLEAN,
+        USER_PROPERTY_TYPE_DATETIME,
     }
 
     companion object {
+        private fun convertToInstant(value: Any): Instant? {
+            return when (value) {
+                is Date -> value.toInstant()
+                is Calendar -> value.toInstant()
+                is ZonedDateTime -> value.toInstant()
+                is LocalDateTime -> value.atZone(ZoneId.systemDefault()).toInstant()
+                is LocalDate -> value.atStartOfDay(ZoneId.systemDefault()).toInstant()
+                is Instant -> value
+                else -> null
+            }
+        }
+
+        private fun formatInstantToIso8601(instant: Instant): String {
+            return instant
+                .atZone(ZoneId.systemDefault())
+                .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+        }
+
         fun of(name: String, value: Any): ClixUserProperty {
             return when (value) {
                 is Boolean ->
@@ -32,11 +59,18 @@ internal data class ClixUserProperty(
                 is String ->
                     ClixUserProperty(name, UserPropertyType.USER_PROPERTY_TYPE_STRING, value)
                 else ->
-                    ClixUserProperty(
-                        name,
-                        UserPropertyType.USER_PROPERTY_TYPE_STRING,
-                        value.toString(),
-                    )
+                    convertToInstant(value)?.let { instant ->
+                        ClixUserProperty(
+                            name,
+                            UserPropertyType.USER_PROPERTY_TYPE_DATETIME,
+                            formatInstantToIso8601(instant),
+                        )
+                    }
+                        ?: ClixUserProperty(
+                            name,
+                            UserPropertyType.USER_PROPERTY_TYPE_STRING,
+                            value.toString(),
+                        )
             }
         }
     }
